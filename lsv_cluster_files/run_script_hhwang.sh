058b9eb8c2aa18d6b1b156145b4af61131007f23
rozariwang <hohsuann@gmail.com>
2024-06-23 13:18:45 +0200

    update sh file

--- lsv_cluster_files/run_script_hhwang.sh
+++ lsv_cluster_files/run_script_hhwang.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-#python /nethome/hhwang/kagaku/ChemBERTa-5M-MLM_finetuning.py
-python ../ChemBERTa-5M-MLM_finetuning.py
+python /nethome/hhwang/kagaku/ChemBERTa-5M-MLM_finetuning.py
 
