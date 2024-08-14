f14619683b69bec27c1a16217c0b6b0ab98f1189
rozariwang <hohsuann@gmail.com>
2024-08-14 13:17:28 +0200

    add mamba_gridsearch.py

---
+++ lsv_cluster_files/run_mamba.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=smiles-training
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+#SBATCH --ntasks=1
+#SBATCH --gpus=1
+#SBATCH --time=02:00:00
+#SBATCH --mem=16G
+#SBATCH --partition=your_partition  # specify if your cluster requires it
+
+# Activate the Conda environment
+source activate mamba
+
+# Run the Python script, adjust the path to where your script is located
+python /hhwang/kagaku/mamba_gridsearch.py
