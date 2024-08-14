5d5c1cb5492f2d4a4f74ae80747e413d94d2ecce
rozariwang <hohsuann@gmail.com>
2024-08-14 14:03:39 +0200

    update file

--- lsv_cluster_files/run_mamba.sh
+++ lsv_cluster_files/run_mamba.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-#SBATCH --job-name=smiles-training
-#SBATCH --output=%x.out
-#SBATCH --error=%x.err
-#SBATCH --ntasks=1
-#SBATCH --gpus=1
-#SBATCH --time=02:00:00
-#SBATCH --mem=16G
-#SBATCH --partition=your_partition  # specify if your cluster requires it
 
-# Activate the Conda environment
 source activate mamba
+python /nethome/hhwang/hhwang/kagaku/mamba_gridsearch.py
 
-# Run the Python script, adjust the path to where your script is located
-python mamba_gridsearch.py
