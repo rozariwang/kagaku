52274e77c4931f1d6b9d3ad3486c67d4117cab28
rozariwang <hohsuann@gmail.com>
2024-08-26 23:51:09 +0200

    update file

--- lsv_cluster_files/run_mamba.sh
+++ lsv_cluster_files/run_mamba.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+# Build Docker image using the provided Dockerfile
+
+# Load Docker module if needed
+module load docker
+
+# Run Docker build command
 docker build -f ./lsv_cluster_files/mamba.dockerfile --build-arg USER_UID=$UID --build-arg USER_NAME=$(id -un) -t docker.lsv.uni-saarland.de/hhwang/mamba:0 .
 
