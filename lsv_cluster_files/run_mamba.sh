b58b6f74c185f98039fbf4b61fdb04192a774e98
rozariwang <hohsuann@gmail.com>
2024-09-07 17:08:48 +0200

    change

--- lsv_cluster_files/run_mamba.sh
+++ lsv_cluster_files/run_mamba.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-# Build Docker image using the provided Dockerfile
-
-# Load Docker module if needed
-module load docker
-
-# Run Docker build command
-docker build -f ./lsv_cluster_files/mamba.dockerfile --build-arg USER_UID=$UID --build-arg USER_NAME=$(id -un) -t docker.lsv.uni-saarland.de/hhwang/mamba:0 .
-
+python /nethome/hhwang/hhwang/kagaku/mamba_gridsearch.py
